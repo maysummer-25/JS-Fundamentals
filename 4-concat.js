931a3296ffba26124a6738f3b23d56da965cd382
may_summer <memechukwumezie@gmail.com>
2025-05-21 20:53:03 +0100

    Spelling mistake corrected

--- 4-concat.js
+++ 4-concat.js
@@ -2,5 +2,5 @@ const program = process.argv[2];
 
 const adjective = process.argv[3];
 
-const fullSentence = `${program} is ${adverb}`;
+const fullSentence = `${program} is ${adjective}`;
 console.log(fullSentence);
