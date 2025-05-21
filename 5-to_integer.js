732ea5bcc7927309d4155c5260861c4c486a44db
may_summer <memechukwumezie@gmail.com>
2025-05-21 21:28:57 +0100

    Remove space to match the expected output

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,7 +3,7 @@ let number = process.argv[2];
 number = parseInt(number);
 
 if(number){
-    console.log("My number: ", number);
+    console.log("My number:", number);
 } else {
     console.log("Not a number");
 }
