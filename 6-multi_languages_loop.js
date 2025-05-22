854aff10b4af58803c1b4fc474d3bf95fb1c2643
may_summer <memechukwumezie@gmail.com>
2025-05-22 13:57:40 +0100

    Add space after the for

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 const sentence = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for(let i = 0; i < sentence.length; i++){
+for (let i = 0; i < sentence.length; i++){
     console.log(sentence[i]);
 }
 
