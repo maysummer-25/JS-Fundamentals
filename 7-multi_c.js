73d2a90693133f48f6041faa08503c82a7b9f317
may_summer <memechukwumezie@gmail.com>
2025-05-23 10:48:45 +0100

    Add 7-multi_c.js script to print “C is fun” x times using a while loop and handle invalid input

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const test = parseInt(process.argv[2]);
+
+if (!isNaN(test)) {
+    let i = 0
+    while(i < test) {
+        console.log("C is fun");
+        i++;
+    }
+} else {
+    console.log("Missing number of occurrences");
+}
